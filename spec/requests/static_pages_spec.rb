574b7d441000496ecd13f4a676877a52f922ea3e
Jake Klassen <klassenja@hotmail.com>
2013-05-09 18:28:05 -0700

    Added new page and refactored tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,21 +1,26 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
+
     it "should have the h1 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('h1', text: 'Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
-    	visit '/static_pages/home'
-    	page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Home')
-    	# could be a simple substring too
-    	# this would match too
-    	# page.should have_selector('title', text: ' | Home')
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
 
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Help page" do
@@ -27,7 +32,7 @@ describe "Static pages" do
 
     it "should have the right title" do
     	visit '/static_pages/help'
-    	page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Help')
+    	page.should have_selector('title', text: "#{base_title} | Help")
     end
 
   end
@@ -41,7 +46,21 @@ describe "Static pages" do
 
     it "should have the right title" do
     	visit '/static_pages/about'
-    	page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | About')
+    	page.should have_selector('title', text: "#{base_title} | About")
+    end
+
+  end
+
+  describe "Contact" do
+    it "should have the h1 'Contact'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "#{base_title} | Contact")
     end
 
   end
