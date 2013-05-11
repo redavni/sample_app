04567e2f7f1eced85caf8bf32f3e0f2b64a2061b
Jake Klassen <klassenja@hotmail.com>
2013-05-10 21:15:13 -0700

    Added helper test and removed duplicate full_title function

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,3 @@
 # Files in the spec/support directory are automatically included by RSpec
 
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
